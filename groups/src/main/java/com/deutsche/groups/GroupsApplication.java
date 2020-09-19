76895eecbcc234f256e4117a30b3271c5dae7a71
artem-chemezov <chemezov.artyom@gmail.com>
2020-09-19 20:59:29 +0300

    fix eureka

--- groups/src/main/java/com/deutsche/groups/GroupsApplication.java
+++ groups/src/main/java/com/deutsche/groups/GroupsApplication.java
@@ -3,6 +3,7 @@ package com.deutsche.groups;
 import com.deutsche.groups.rabbit.RabbitConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 @EnableAsync
 @Import(RabbitConfiguration.class)
+@EnableEurekaClient
 public class GroupsApplication {
 
 
